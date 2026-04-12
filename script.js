7f98819647545cebce86e34354333172a63f9d39
abhayrana7281-ai <abhayrana7281@gmail.com>
2026-04-12 21:45:52 +0530

    download section

--- script.js
+++ script.js
@@ -1,31 +1,12 @@
-const body = document.body;
-const btn = document.querySelector("button");
-const media = window.matchMedia("(prefers-color-scheme: dark)");
+let count = 0;
+let progress = document.querySelector(".progress");
+let percent = document.querySelector("#percent");
+setInterval(function(){
+    if(count<=99){
+        count++;
+        progress.style.width = count + "%";
+        percent.textContent = count + "%";
 
-function applyTheme(theme) {
-    body.classList.remove("dark", "light");
-    body.classList.add(theme);
-}
 
-// Initial load
-let savedTheme = localStorage.getItem("theme");
-
-if (savedTheme) {
-    applyTheme(savedTheme);
-} else {
-    applyTheme(media.matches ? "dark" : "light");
-}
-
-// System change (only if no manual theme)
-media.addEventListener("change", () => {
-    if (!localStorage.getItem("theme")) {
-        applyTheme(media.matches ? "dark" : "light");
     }
-});
-
-// Toggle button
-btn.addEventListener("click", () => {
-    const newTheme = body.classList.contains("dark") ? "light" : "dark";
-    applyTheme(newTheme);
-    localStorage.setItem("theme", newTheme);
-});
+},30);
